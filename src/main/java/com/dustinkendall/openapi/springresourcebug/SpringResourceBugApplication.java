22f5e57747291e3fb6eb0ee73dcad7192114624d
Dustin Kendall <dustin.kendall@wexinc.com>
2022-05-24 22:56:51 -0400

    Initial Commit

---
+++ src/main/java/com/dustinkendall/openapi/springresourcebug/SpringResourceBugApplication.java
@@ -0,0 +1,13 @@
+package com.dustinkendall.openapi.springresourcebug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResourceBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResourceBugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dustinkendall/openapi/springresourcebug/SpringResourceBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dustinkendall.openapi.springresourcebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResourceBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
